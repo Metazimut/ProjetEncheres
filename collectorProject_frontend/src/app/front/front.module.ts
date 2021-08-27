1a9a861b1b5ee174b158da306d2bc8a1b290a7bc
Mathieu W <w.mathieu.dk59@gmail.com>
2021-08-27 17:31:38 +0200

    v2

--- collectorProject_frontend/src/app/front/front.module.ts
+++ collectorProject_frontend/src/app/front/front.module.ts
@@ -1,26 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PlanningComponent } from './planning/planning.component';
-import {RdvFuturComponent} from "./rdv-futur/rdv-futur.component";
-import {ConsultationComponent} from "./consultation/consultation.component";
-import { CreneauComponent } from './creneau/creneau.component';
-import {PatientComponent} from "./patient/patient.component";
-import {LieuComponent} from "./lieu/lieu.component";
-import {AccueilComponent} from "./accueil/accueil.component";
-import {PraticienComponent} from "./praticien/praticien.component";
 
 
 
 @NgModule({
   declarations: [
-    PlanningComponent,
-    RdvFuturComponent,
-    ConsultationComponent,
-    CreneauComponent,
-    PatientComponent,
-    LieuComponent,
-    AccueilComponent,
-    PraticienComponent,
   ],
   imports: [
     CommonModule
