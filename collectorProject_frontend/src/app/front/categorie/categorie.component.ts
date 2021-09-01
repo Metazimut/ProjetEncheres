14906d34ecc7cd7a6207ef532ec031b69d5934ab
Unknown <aurelien.dispot@gmail.com>
2021-09-01 17:19:59 +0200

    cc

--- collectorProject_frontend/src/app/front/categorie/categorie.component.ts
+++ collectorProject_frontend/src/app/front/categorie/categorie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CategorieService} from "./categorie.service";
 import {Categorie} from "../../model/categorie";
 import {ActivatedRoute} from "@angular/router";
+import {SessionService} from "../../session.service";
 
 @Component({
   selector: 'app-categorie',
@@ -11,13 +12,17 @@ import {ActivatedRoute} from "@angular/router";
 export class CategorieComponent implements OnInit {
   categorie: Categorie = new Categorie();
 
-  constructor(private categorieService:CategorieService, private route: ActivatedRoute) { }
+  constructor(private categorieService:CategorieService, private route: ActivatedRoute, private sessionService: SessionService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.categorie.id = params.id;
       this.loadCategorieByidWithPublication(params.id);
     })
+  }
+
+  isConnected(): boolean {
+    return this.sessionService.user != null;
   }
 
   loadCategorieByidWithPublication(id:number)  {
